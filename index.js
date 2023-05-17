48799f51009b9b7111c3e3fbaa88fee068afaa75
moneko <poi.nyaa@qq.com>
2023-05-17 09:49:18 +0800

    feat: prettier

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
@@ -177,57 +179,11 @@ module.exports = {
     'spaced-comment': 2, // 注释风格要不要有空格什么的
     'valid-jsdoc': 2, // jsdoc规则
     'wrap-regex': 0, // 正则表达式字面量用小括号包起来
-    // 不允许连续空格
-    'no-multi-spaces': 'error',
-    // 空2格 switch case
-    indent: ['error', 2, { SwitchCase: 1 }],
-    // 对象大括号空格 {   a:b   } => { a:b }
-    'object-curly-spacing': ['error', 'always'],
-    // 括号去除空格 foo(   'bar'   ) =>  foo('bar');
-    'space-in-parens': ['error', 'never'],
-    // 对象分号前后只有一个空格{ "foo"  :    42 } => { "foo": 42 };
-    'key-spacing': ['error', { mode: 'strict' }],
-    'comma-spacing': ['error', { before: false, after: true }],
-    // if else 风格
-    'brace-style': ['error', '1tbs'],
-    // 函数调用空格 fn  () => fn()
-    'func-call-spacing': ['error', 'never'],
-    // 函数左括号空格 function name         () {} => function name(){}
-    'space-before-function-paren': ['error', 'never'],
-    // 语句块的空格 function name() {} => function name(){}
-    'space-before-blocks': ['error', 'never'],
-    // 关键字前后空格 if  () => if()
-    'keyword-spacing': [
-      'error',
-      {
-        overrides: {
-          if: { after: false, before: false },
-          else: { after: false, before: false },
-        },
-      },
-    ],
-    // 对象取值不能有空格 obj  .  foo => obj.foo
-    'no-whitespace-before-property': 'error',
-    // 代码块中去除前后空行
-    'padded-blocks': ['error', 'never'],
-    // ;前后空格 var foo   ;   var bar; => var foo;var bar;
-    'semi-spacing': ['error', { before: false, after: false }],
-    // 操作符是否空格 a=0 => a = 0
-    'space-infix-ops': 'error',
-    // 操作符空格 + -
-    'space-unary-ops': 'error',
-    // 扩展运算符 {... f} => {...f}
-    'rest-spread-spacing': 'error',
-    // 字符串拼接使用模版字符串 'hello' + world => `hello${world}`
-    'prefer-template': 'error',
-    // 模版字符串中去除空格 `${ fo  }` =>${fo}
-    'template-curly-spacing': ['error', 'never'],
-    // 链式调用换行
-    'newline-per-chained-call': ['error', { ignoreChainWithDepth: 1 }],
+    'prefer-template': 2, // 字符串拼接使用模版字符串 'hello' + world => `hello${world}`
     // 禁止重复模块导入
-    'no-duplicate-imports': 'error',
+    'no-duplicate-imports': 2,
     // 使用单引号，字符串中包含了一个其它引号 允许"a string containing 'single' quotes"
-    quotes: ['error', 'single', { avoidEscape: true }],
+    quotes: [2, 'single', { avoidEscape: true }],
   },
   globals: {
     routeBaseName: 'readonly',
