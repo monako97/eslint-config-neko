ebbb39ca876ed8e13a249885d26c12875fb91038
moneko97 <poi.nyaa@qq.com>
2024-06-27 16:32:05 +0800

    ts 5.5

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const baseConfig = {
     sourceType: "module",
   },
   // 定义了该eslint文件所依赖的插件
-  plugins: ["@typescript-eslint", "html", "import"],
+  plugins: ["@typescript-eslint", "import"],
   // "off" 或 0 - 关闭规则
   // "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出)
   // "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
